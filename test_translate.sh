8aa879502006133a0688560794c51204bd32b2a0
auliaadila <auliaadila36@gmail.com>
2026-03-01 15:26:42 +0700

    feat: add analysis on which data fields to flatten (utils/db_analyze_flatten.py)

--- test_translate.sh
+++ test_translate.sh
@@ -1,9 +1,22 @@
-## bash
+## DOMAIN 1: AIRLINE
+# uv run utils/db_analyze_flatten.py data/tau2/domains/airline/db.json --detail
 
-uv run utils/db_json_to_excel.py --input data/tau2/domains/airline/db.json \
-  --output data/excel_database/airline_db.xlsx \
+## v1: flatten users.name, users.address, users.saved_passengers, reservations.passengers
+# uv run utils/db_json_to_excel.py --input data/tau2/domains/airline/db.json \
+#   --output data/excel_database/airline_db.xlsx \
+#   --flatten users.name \
+#   --flatten users.address \
+#   --flatten users.saved_passengers \
+#   --flatten reservations.passengers \
+#   --overwrite -v
+
+uv run utils/db_json_to_excel.py \
+    -i data/tau2/domains/airline/db.json \
+    -o data/excel_database/airline_db.xlsx \
   --flatten users.name \
   --flatten users.address \
   --flatten users.saved_passengers \
+  --flatten reservations.flights \
   --flatten reservations.passengers \
+  --flatten reservations.payment_history \
   --overwrite -v
